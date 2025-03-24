dc9d216dd97bbc23b094a42860cb1b4a3a1808c2
mfraanje <150241076+mfraanje@users.noreply.github.com>
2025-03-24 10:01:11 +0100

    Create subscriptionValidatorTest.ts

---
+++ src/functions/subscriptionValidatorTest.ts
@@ -0,0 +1,19 @@
+import { app } from "@azure/functions";
+import type { HttpRequest, InvocationContext } from "@azure/functions";
+
+export async function subscriptionValidatorTest(
+    req: HttpRequest,
+    context: InvocationContext
+): Promise<any> {
+    context.log(req.body);
+    return {
+        status: 200,
+        body: { subscription: false }
+    };
+}
+
+app.http('subscriptionvalidatortest', {
+    route: 'subscription/validator/test',
+    handler: subscriptionValidatorTest,
+    authLevel: 'anonymous'
+});
