402ef2331def0e3d17b4533c928744d7410394dd
mfraanje <150241076+mfraanje@users.noreply.github.com>
2025-04-11 15:30:31 +0200

    Update monitorSubscription.ts

--- src/functions/monitorSubscription.ts
+++ src/functions/monitorSubscription.ts
@@ -1,24 +1,26 @@
-import { app } from "@azure/functions";
+import {app} from "@azure/functions";
+import type { InvocationContext, Timer } from "@azure/functions";
 import createMollieClient, { SequenceType } from '@mollie/api-client';
 
 const MOLLIE_API_KEY = process.env.MOLLIE_API_KEY as string;
 
-async function monitorSubscription() {
-  console.log('monitorSubscription function started');
+async function monitorSubscription(context: InvocationContext) {
+  context.info('monitorSubscription function started');
   const mollieClient = createMollieClient({ apiKey: MOLLIE_API_KEY });
 
   try {
     const subscriptionsawait = await mollieClient.subscription.page();
     for (const subscription of subscriptionsawait) {
-      console.log(subscription);
+      context.info(subscription);
     }
   } catch (error) {
-    console.error('Error in monitorSubscription function:', error);
+    context.error('Error in monitorSubscription function:', error);
   }
 }
 
 // Register the function with Azure Functions
 app.timer('monitorSubscription', {
   schedule: '0 */1 * * * *',
-  handler: monitorSubscription
+  runOnStartup: true,
+  handler: async (myTimer: Timer) => {monitorSubscription}
 });
